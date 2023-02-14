9bcc24fb10a7af17a03aa3c54419b23b94e7b735
Brandon Wilk <bwilk777@uab.edu>
2023-02-14 08:36:05 -0600

    switching up install order b/c deps are dumb

--- src/fix_ffpe_vars.R
+++ src/fix_ffpe_vars.R
@@ -1,4 +1,4 @@
-# A simple script to process VCF files and produce FFPE induced variants from the VCFs for input into SigProfiler
+# A simple script to process VCF files and produce FFPE induced variants from the VCFs for input into SigProfiler # nolint
 
 suppressPackageStartupMessages(library("argparse"))
 
@@ -10,7 +10,7 @@ parser <- ArgumentParser()
 parser$add_argument("-i", "--input_dir",
     type = "character",
     metavar = "PATH",
-    help = "Path to the directory containing input VCF file(s) to fix for FFPE mutations"
+    help = "Path to the directory containing input VCF file(s) to fix for FFPE mutations" # nolint
 )
 
 parser$add_argument("-o", "--output_dir",
@@ -22,12 +22,12 @@ parser$add_argument("-o", "--output_dir",
 parser$add_argument("-r", "--ref_genome",
     type = "character",
     metavar = "PATH",
-    help = "Path to the FASTA file of the reference genome variants were called from"
+    help = "Path to the FASTA file of the reference genome variants were called from" # nolint
 )
 
 parser$add_argument("-s", "--setup",
     action = "store_true", default = FALSE,
-    help = "Install needed R libraries to process things (only needed after Conda env is built fresh)"
+    help = "Install needed R libraries to process things (only needed after Conda env is built fresh)" # nolint
 )
 
 # get command line options, if help option encountered print help and exit,
@@ -36,14 +36,20 @@ args <- parser$parse_args()
 
 if (args$setup) {
     print("Running setup...")
-    ##### Needed for specific model version used by the tool and noted by failure of Vignette to build #####
+    ##### Needed for specific model version used by the tool and noted by failure of Vignette to build ##### # nolint
     # install the language server for vscode
-    if (!require("languageserver")) install.packages("languageserver", repos = "http://cran.us.r-project.org")
-    if (!require("httpgd")) install.packages("httpgd", repos = "http://cran.us.r-project.org")
-    # specific version release info https://h2o-release.s3.amazonaws.com/h2o/rel-xu/1/index.html
-    if (!require("devtools")) install.packages("devtools", repos = "http://cran.us.r-project.org")
+    if (!require("languageserver")) install.packages("languageserver", repos = "http://cran.us.r-project.org") # nolint
+    if (!require("httpgd")) install.packages("httpgd", repos = "http://cran.us.r-project.org") # nolint
+    # specific version release info https://h2o-release.s3.amazonaws.com/h2o/rel-xu/1/index.html # nolint
+    if (!require("devtools")) install.packages("devtools", repos = "http://cran.us.r-project.org") # nolint
 
-    # The following two commands remove any previously installed H2O packages for R.
+    # setup ideafix
+    library(devtools)
+    library("stringr")
+    library(h2o)
+    devtools::install_github("mmaitenat/ideafix", build_vignettes = FALSE)
+
+    # The following two commands remove any previously installed H2O packages for R. # nolint
     if ("package:h2o" %in% search()) {
         detach("package:h2o", unload = TRUE)
     }
@@ -59,20 +65,11 @@ if (args$setup) {
         }
     }
 
-    # Now we download, install and initialize the H2O package for R.
-    install.packages("h2o", type = "source", repos = "https://h2o-release.s3.amazonaws.com/h2o/rel-xu/1/R")
-
-    # setup ideafix
-    library(devtools)
-    library("stringr")
-    library(h2o)
-    h2o.init()
-    devtools::install_github("mmaitenat/ideafix", build_vignettes = FALSE)
-    # Shutdown H20 cluster when things are done processing
-    h2o.shutdown(prompt = FALSE)
+    # Now we downloadand install the H2O package for R.
+    install.packages("h2o", type = "source", repos = "https://h2o-release.s3.amazonaws.com/h2o/rel-xu/1/R") # nolint
 } else {
-    if (is.null(args$input_dir) || is.null(args$output_dir) || is.null(args$ref_genome)) {
-        write("Input and Output directory and referece genome paths must be specified!\n", stderr())
+    if (is.null(args$input_dir) || is.null(args$output_dir) || is.null(args$ref_genome)) { # nolint
+        write("Input and Output directory and referece genome paths must be specified!\n", stderr()) # nolint
         stop("Incorrect Input")
     }
 
@@ -83,26 +80,26 @@ if (args$setup) {
     h2o.init()
 
     if (!dir.exists(args$output_dir)) dir.create(args$output_dir)
-    vcf_files <- list.files(args$input_dir, pattern = ".+\\.vcf", full.names = TRUE)
+    vcf_files <- list.files(args$input_dir, pattern = ".+\\.vcf", full.names = TRUE) # nolint
     for (vcf in vcf_files) {
         cat("Fixing ", basename(vcf), "\n")
         # get the descriptors for the variants using the VCF and ref genome
-        descriptors <- get_descriptors(vcf_filename = vcf, fasta_filename = args$ref_genome)
+        descriptors <- get_descriptors(vcf_filename = vcf, fasta_filename = args$ref_genome) # nolint
 
         # predict with the random forest model
-        predictions_RF <- classify_variants(variant_descriptors = descriptors, algorithm = "RF")
+        predictions_RF <- classify_variants(variant_descriptors = descriptors, algorithm = "RF") # nolint
 
         # annotate deamination info filag to vcf for downstream filtering
         outname <- str_replace(basename(vcf), ".vcf", ".ideafix")
-        annotate_deaminations(classification = predictions_RF, format = "vcf", vcf_filename = vcf, outfolder = args$output_dir, outname = outname)
+        annotate_deaminations(classification = predictions_RF, format = "vcf", vcf_filename = vcf, outfolder = args$output_dir, outname = outname) # nolint
 
-        # use BCFTools to filter out the deamination flagged variants from the VCF
-        output_vcf <- file.path(args$output_dir, paste(outname, ".vcf", sep = ""))
-        cmd <- sprintf("bcftools view --exclude 'INFO/DEAMINATION=\"deamination\"' %s -Ov -o %s_tmp.vcf; mv %s_tmp.vcf %s", output_vcf, output_vcf, output_vcf, output_vcf)
+        # use BCFTools to filter out the deamination flagged variants from the VCF # nolint
+        output_vcf <- file.path(args$output_dir, paste(outname, ".vcf", sep = "")) # nolint
+        cmd <- sprintf("bcftools view --exclude 'INFO/DEAMINATION=\"deamination\"' %s -Ov -o %s_tmp.vcf; mv %s_tmp.vcf %s", output_vcf, output_vcf, output_vcf, output_vcf) # nolint
         system(cmd, intern = TRUE)
 
         # log the number of variants removed due to predicted deamination
-        cat(format(sum(predictions_RF$DEAMINATION == "deamination")), "variants predicted to be deamination induced and removed from", output_vcf, "\n")
+        cat(format(sum(predictions_RF$DEAMINATION == "deamination")), "variants predicted to be deamination induced and removed from", output_vcf, "\n") # nolint
     }
 
     # Shutdown H20 cluster when things are done processing
